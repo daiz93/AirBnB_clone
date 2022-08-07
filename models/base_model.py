e2b7f939d71b5e4bcb56e43b9e213a1ddf39ce13
daiz93 <desmond.kpo@gmail.com>
2022-08-07 23:03:26 +0100

    Task 3

--- models/base_model.py
+++ models/base_model.py
@@ -3,30 +3,36 @@
 # importing datetime module for uuid4()
 import uuid
 # importing datetime module for now()
-import datetime
+from datetime import datetime
 
 class BaseModel:
-'''
-defines all common attributes/methods for other classes
-'''
+    '''
+    defines all common attributes/methods for other classes
+    '''
 
-    id = str(uuid.uuid4())
 
     def __init__(self):
-    '''Initialze instance'''
-        self.created_at = datetime.datetime.now()
-        self.updated_at = datetime.datetime.now()
+        '''Initialze instance'''
+        self.id = str(uuid.uuid4())
+        self.created_at = datetime.now()
+        self.updated_at = datetime.now()
     
     def __str__(self):
         ''' print: [<class name>] (<self.id>) <self.__dict__>
         '''
-        return '[' + type(self).__name__ + '] (' + str(self.id) + ') ' + self.__dict__
+        return '[' + type(self).__name__ + '] (' + str(self.id) + ') ' + str(self.__dict__)
 
     def save(self):
         '''updates the public instance attribute updated_at with the current datetime
         '''
-        self.updated_at = datetime.datetime.now()
+        self.updated_at = datetime.now()
     def to_dict(self):
-        '''returns a dictionary containing all keys/values of __dict__ of the instance
+        '''returns a dictionary containing all keys
+        /values of __dict__ of the instance
         '''
-        
+        dict_dictdictionary = self.__dict__.copy()
+        dict_dictdictionary["__class__"] = self.__class__.__name__
+        dict_dictdictionary["created_at"] = dict_dictdictionary["created_at"].isoformat()
+        dict_dictdictionary["updated_at"] = dict_dictdictionary["updated_at"].isoformat()
+        return dict_dictdictionary
+
