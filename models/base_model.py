76892a0b74caa9a33f5de2cbe336782a0e56ec9f
daiz93 <desmond.kpo@gmail.com>
2022-08-09 22:59:28 +0100

    Task 4

--- models/base_model.py
+++ models/base_model.py
@@ -11,12 +11,19 @@ class BaseModel:
     '''
 
 
-    def __init__(self):
+    def __init__(self, *args, **kwargs):
         '''Initialze instance'''
         self.id = str(uuid.uuid4())
         self.created_at = datetime.now()
         self.updated_at = datetime.now()
-    
+        if (kwargs is not None):
+            for key, value in kwargs.items():
+                if key != '__class__':
+                    if key in ['created_at','updated_at']
+                        self.__dict__[key] = datetime.strptime(value, "%Y-%m-%dT%H:%M:%S.%f")
+                    else:
+                        self.__dict__[key] = value
+
     def __str__(self):
         ''' print: [<class name>] (<self.id>) <self.__dict__>
         '''
