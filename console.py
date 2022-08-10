7553c548b81742f5545cacff723827c867903e81
daiz93 <desmond.kpo@gmail.com>
2022-08-11 00:02:27 +0100

    Pycode style

--- console.py
+++ console.py
@@ -1,18 +1,20 @@
 #!/usr/bin/python3
+"""contains the entry point of the command interpreter:"""
 import cmd
 
+
 class HBNBCommand(cmd.Cmd):
-    ''' Commande interpreter
-    Args : 
-        cmd
+    '''HBNB Consol
+    Attr:
+        custom_prompt (str): command line prompt
     '''
     prompt = '(hbnb)'
 
-    def do_quit(self,args):
+    def do_quit(self, args):
         """Quit command to exit the program"""
         return True
 
-    def do_EOF(self,args):
+    def do_EOF(self, args):
         '''Exit the program'''
         print("")
         return True
@@ -21,6 +23,7 @@ class HBNBCommand(cmd.Cmd):
         ''' an empty line + ENTER shouldn’t execute anything
         '''
         pass
+
 
 if __name__ == '__main__':
     HBNBCommand().cmdloop()
