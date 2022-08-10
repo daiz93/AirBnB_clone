bf927b713b73b33c239f5fa83f2926428c9e09f4
daiz93 <desmond.kpo@gmail.com>
2022-08-10 23:53:20 +0100

    Add consol

--- console.py
+++ console.py
@@ -1,37 +1,25 @@
-# contains the entry point of the command interpreter
-
-import cmd, sys
-from turtle import *
+#!/usr/bin/python3
+import cmd
 
 class HBNBCommand(cmd.Cmd):
-    '''
-    Init the consol
-    Ars:
-        Cmd:
+    ''' Commande interpreter
+    Args : 
+        cmd
     '''
     prompt = '(hbnb)'
-    file = None
 
-    def do_quit(self):
-        ''' exit the program
-        '''
-        self.close()
-        bye()
+    def do_quit(self,args):
+        """Quit command to exit the program"""
         return True
 
     def do_EOF(self,args):
-        ''' exit the program
-        '''
-        if self.file:
-            self.file.close()
-            self.file = None
+        '''Exit the program'''
+        print("")
         return True
 
-    def do_help(self):
-        pass
-
     def do_emptyline(self):
-        '''an empty line + ENTER shouldn’t execute anything'''
+        ''' an empty line + ENTER shouldn’t execute anything
+        '''
         pass
 
 if __name__ == '__main__':
